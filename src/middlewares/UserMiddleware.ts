04903765fc63423d592066c2cc9fd6b6ca01afdf
sirguy12 <ryankairo09@yahoo.com.br>
2023-07-10 00:38:30 -0300

    eye :refactor:

--- src/middlewares/UserMiddleware.ts
+++ src/middlewares/UserMiddleware.ts
@@ -4,29 +4,33 @@ import {Users} from '../models/usersModel'
 
 export const CheckToken = async (req:Request,res:Response,next:NextFunction)=>{
     // if (req.query.token) return res.status(401).json({message:'O token deve ser enviado apenas em formado JSON'})
-    let token;
+    let token:string;
 
-    if (!req.body.token && !req.query.token) return res.status(401).json({message:'Usuário não forneceu o token de acesso'})
-    
-    
+    if (req.query.token) {
+      token = req.query.token as string;
+    } else if (req.body.token) {
+      token = req.body.token as string;
+    } else {
+      return res.status(401).json({ message: 'Usuário não forneceu o token de acesso' });
+    }
 
-    if (req.body.token && req.body._token.length > 50){
-        try{
-            let token = await Users.findOne({
-                where:{
-                    token:req.body.token
-                }
-            })
+    if (token.length < 10) return res.status(401).json({ message: 'Não parece ser um token válido' }); 
 
-            return res.status(200).json({message:token})
-
+    try{
+        let user = await Users.findOne({
+            where:{
+                token:token
+            }
+        })
+        if (user){
+            return next()
         }
-        catch(err){
-            return res.status(400).json({message:'Token invalido'})
-        }
-
-
+        return res.status(400).json({message:'Usuário com o token específico não foi encontrado'})
 
     }
+    catch(err){
+        return res.status(400).json({message:'Erro interno'})
+    }
+    
 
 }
