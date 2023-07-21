bad42ec7016d9df4b9b131991c966b4953762ac9
sirguy12 <ryankairo09@yahoo.com.br>
2023-07-21 14:09:43 -0300

    feat: signin option

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -7,7 +7,35 @@ import { validationResult,matchedData } from "express-validator"
 
 
 const signin = async (req:Request,res:Response)=>{
+    const errors = validationResult(req)
+    if (!errors.isEmpty()){
+        return res.status(400).json({message:errors.mapped()})
+    }
+    const data = matchedData(req)
+    
+    const userFind = await Users.findOne({
+        where:{
+            email:data.email
+        }
+    })
 
+
+    if (!userFind){
+        return res.status(404).json({message:'Usuário ou senha incorreto'})
+    }
+    const checkPass = await bcrypt.compare(data.password,userFind?.passHashed)
+    if(checkPass){
+        const payload = (Date.now() + Math.random()).toString()
+        const token = await bcrypt.hash(payload,10)
+        userFind.token = token
+        userFind.save()
+        return res.json({message:'Logado com sucesso!',
+        name:userFind.name,
+        email: userFind.email,
+        token: userFind.token})
+
+    }
+    return res.status(404).json({message:'Usuário ou senha incorreto'})
 }
 const signup = async (req:Request,res:Response)=>{
     const errors = validationResult(req)
@@ -42,19 +70,22 @@ const signup = async (req:Request,res:Response)=>{
     const token = await bcrypt.hash(payload,10)
 
     try{
-        await Users.create({
+        const user = await Users.create({
         name:data.name,
         email:data.email,
         state:data.state,
         passHashed:passHash,
         token})
+
+
+        return res.json({message:'tudo ok',data, token:user.token})
     }
     catch(err){
         return res.status(400).json({message:'Erro na criação do usuário'})
     }
 
 
-    res.json({message:'tudo ok',data})
+    
     
 }
 
